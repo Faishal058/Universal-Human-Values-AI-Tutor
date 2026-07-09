e3d3c4c56d96ab058e5f034273e3c3f9ff831856
Faishal058 <faishalrahman729580@gmail.com>
2026-07-09 20:44:40 +0530

    Fix ChatGroq NameError by adding global imports

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
 
 # -----------------------------------------------------------------------------
 # Configuration & Theme
